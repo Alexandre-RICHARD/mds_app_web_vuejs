0f1dc1fd4c5cf29eaaaed423992a0f8696a4f034
Alexandre RICHARD <arichard@predell.fr>
2025-06-04 14:24:50 +0200

    Change favicon

--- src/views/components/AppHeader.vue
+++ src/views/components/AppHeader.vue
@@ -1,13 +1,10 @@
-<script setup lang="ts">
-import LogoSVG from "../svg/LogoSVG.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <v-app-bar
     flat
     class="px-4 bg-gray-200"
   >
-    <LogoSVG class="mr-2" />
     <span class="font-bold">MDS App</span>
   </v-app-bar>
 </template>
